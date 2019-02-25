a8766e2d9a0f9b4949ba24a33f8c98d34f4bfe29
Nazari <ryannazari21@gmail.com>
2019-02-25 12:41:25 +0200

    Add image and label in price view controller

--- Uber/Controllers/PriceViewController.swift
+++ Uber/Controllers/PriceViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class PriceViewController: UIViewController {
 
-    @IBOutlet weak var priceTextField: UITextField!
+
+    var imgArray = ["car1", "car2", "car3", "car4"]
+    let random1 = Int.random(in: 0...3)
+    let random2 = Int.random(in: 1...5)
+    
+    @IBOutlet weak var carImg: UIImageView!
+    @IBOutlet weak var pticeTextfield: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        carImg.image = UIImage(named: imgArray[random1])
+        pticeTextfield.text = "Driver is comming, You should pay $\(random2*2)"
     }
-    
-
 
 }
